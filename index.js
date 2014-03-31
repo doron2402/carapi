6e7f511332f6bad38b2f537b0f0157093fd5a04c
Doron Segal <doron2402@gmail.com>
2014-03-30 23:22:44 -0700

    adding mongo adapter

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var Hapi = require("hapi");
 var APP = {};
 APP.PORT = process.env.PORT || 5000;
 
-
 var dbOpts = {
-    "url": "mongodb://localhost:27017/test",
+    "url": process.env['MONGOHQ_URL'] ? process.env['MONGOHQ_URL'] : "mongodb://localhost:27017/test",
     "options": {
         "db": {
             "native_parser": false
@@ -23,34 +22,51 @@ server.pack.require('hapi-mongodb', dbOpts, function(err) {
 
 server.route( [{
     "method"  : "GET",
-    "path"    : "/data/{id}",
+    "path"    : "/data/get",
     "handler" : getData
 },{
     "method"  : "POST",
-    "path"    : "/data/{id}",
+    "path"    : "/data/post",
     "handler" : postData
 },{
     "method"  : "GET",
-    "path"    : "/test/{id}",
+    "path"    : "/test/get/{id}",
     "handler" : testRoute
 }]);
 
 function usersHandler(request, reply) {
     var db = request.server.plugins['hapi-mongodb'].db;
     var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
-    db.collection('users').findOne({  "_id" : new ObjectID(request.params.id) }, function(err, result) {
+    db.collection('data').findOne({  "_id" : new ObjectID(request.params.id) }, function(err, result) {
         if (err) return reply(Hapi.error.internal('Internal MongoDB error', err));
         reply(result);
     });
 };
 
 function postData (request, reply) {
+    var db = request.server.plugins['hapi-mongodb'].db;
+    var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
+    console.error(request.payload);
+    var collection = db.collection('data');
+    collection.insert(request.payload, function(err, docs) {
+        reply({docs: docs, code: "OK"});
+    });
 
-}
+};
 
 //Return all data in the colleciton
 function getData (request, reply) {
+    var db = request.server.plugins['hapi-mongodb'].db;
+    var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
 
+    db.collection('data').find().toArray(function(err, results) {
+        if (err)
+            console.error(err);
+
+        reply({docs: results, code:'OK'});
+        // Let's close the db
+        db.close();
+      });
 };
 
 function testRoute (request, reply) {
